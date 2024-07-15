fdacbc134c890e50527cbd1f01cc8b10434a77b7
RyanFin <ryanfinlayson125@hotmail.com>
2024-07-15 10:55:25 +0100

    add reducer

--- src/Store.js
+++ src/Store.js
@@ -1,30 +1,42 @@
 import { createStore } from "redux";
 
 const initialState = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: '',
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
 };
 
 // set a default parameter
-function reducer(state = initialState, action){
-    switch(action.type){
-        // state domain and event name
-        case "account/deposit":
-            return  {...state, balance: state.balance + action.payload};
-        case "account/withdraw":
-            return  {...state, balance: state.balance - action.payload};
-        case "account/requestLoan":
-            if (state.loan > 0) return state;
-            return {...state, loan: action.payload};
-        case "account/payLoan":
-            return {...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan};
-        
-            default:
-                return state;   
-    }
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    // state domain and event name
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
 }
 
 const store = createStore(reducer);
 
-store.dispatch({type: 'account/deposit', payload: 500});
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 150 });
+console.log(store.getState());
+
+// store.dispatch(type:'account/requestLon', payload: {amunt: 1000, purpose: "buy a car"})
